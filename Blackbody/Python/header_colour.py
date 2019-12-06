7f665342f5d70bfefcda6dfc680ebf0d220a3509
Michael <mweinold@ethz.ch>
2019-12-06 13:03:36 +0000

    Initial commit

---
+++ Blackbody/Python/header_colour.py
@@ -0,0 +1,27 @@
+# PACKAGES
+# =====================================================================
+
+# Plotting
+# =====================================================================
+import matplotlib # For advanced plotting
+import matplotlib.pyplot as plt # (subpackage has to be imported explicitly)
+#import tikzplotlib # For export of figures to LaTeX pgfplot
+
+# Data Science
+# =====================================================================
+import math
+import numpy as np
+import pandas as pd
+import scipy
+import scipy.interpolate as interpolate # (subpackage has to be imported explicitly)
+
+# File Handling
+# =====================================================================
+import os # For I/O and path handling
+import pathlib # For advanced path handling
+import csv # For CSV file import and manipulation
+
+# Colour Science
+# =====================================================================
+import colour
+import colour.plotting # (subpackage has to be imported explicitly)
